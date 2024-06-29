892258d4504d44567f6427632d9f7e79fa8df787
Bismark Boateng <bismarkb609@gmail.com>
2024-06-29 17:15:40 +0000

    fix layout issue at onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -7,7 +7,6 @@ export default async function Onboarding() {
   const currentUser: any = await getUserById()
 
   const userId = await getUserId()
-  console.log(userId)
 
   const parsedCurrentUser = JSON.parse(currentUser)
 
@@ -17,7 +16,7 @@ export default async function Onboarding() {
 
   return (
     <section>
-     <section className="w-[40%] mx-auto mt-7">
+     <section className="w-[90%] md:w-[60%] lg:w-[50%] xl:w-[40%] 2xl:w-[40%] mx-auto mt-7">
       <h1 className="text-2xl font-extrabold">Onboarding</h1>
       <p className="text-base mb-10">
        Complete your profile now to use Giggle
